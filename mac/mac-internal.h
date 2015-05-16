196679bfa96e55d4669404c9291d17ba35c9c873
Andrew Keesler <andrew.keesler@silabs.com>
2015-05-16 12:13:11 -0400

    SNET RENAME: do the deed - remove the old sdl-* files.
    - These files were used to sit at the bottom of a single
      network that was a single process.

--- mac/mac-internal.h
+++ mac/mac-internal.h
@@ -8,9 +8,3 @@
 // Internal definitions for the SDL MAC.
 
 #include <stdint.h>
-
-int SDL_NETWORK_UP(void);
-int SDL_NETWORK_DOWN(void);
-
-// Reset the interface ids.
-void sdlResetInterfaceIds(void);
